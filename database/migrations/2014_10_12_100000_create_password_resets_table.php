41cd39e82cbd27b2472718ed7638714e00f05d54
Alex Renoki <rennokki@gmail.com>
2020-09-17 18:08:38 +0300

    set sql_require_primary_key=0

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreatePasswordResetsTable extends Migration
@@ -13,6 +14,8 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
+        DB::statement('SET SESSION sql_require_primary_key=0');
+
         Schema::create('password_resets', function (Blueprint $table) {
             $table->string('email')->index();
             $table->string('token');
